dfeeaf94f8a1271bf530d7804816fee00882412c
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-09-03 10:42:50 +0300

    Приступил к выполнению ДЗ к уроку №10: "Деревья, рекурсия". Переименовал значения полей и параметров для избавления от антипаттерна "Crytic Code".

--- Homework/Lesson10/index.php
+++ Homework/Lesson10/index.php
@@ -17,72 +17,90 @@ class BinaryNode
 
 class BinaryTree
 {
-    public ?BinaryNode $node;
+    public ?BinaryNode $root;
     public $array;
 
     /**
-     * @param  BinaryNode  $node
+     * @param  BinaryNode  $root
      * @param $array
      */
     public function __construct()
     {
-        $this->node = null;
+        $this->root = null;
         $this->array = null;
     }
 
     public function addNode($value)
     {
-        $item = new BinaryNode($value);
-        if (is_null($this->node)) {
-            $this->node = $item;
+        //Создаём новый узел бинарного дерева из полученного значения
+        $newNode = new BinaryNode($value);
+
+        //Если корневой узел текущего дерева ещё не существует, то помещаем в него новый узел
+        if (is_null($this->root)) {
+            $this->root = $newNode;
             return;
         }
-        $this->_addNode($this->node, $item);
+
+     /* Если корневой узел текущего дерева уже занят, то вызываем метод, который  рекурсивно пытается добавить новый узел в поддеревья текущего дерева */
+        $this->_addNode($this->root, $newNode);
     }
 
-    private function _addNode(BinaryNode $node, BinaryNode $item)
+    private function _addNode(BinaryNode $root, BinaryNode $newNode)
     {
+        //Если значение нового узла меньше, чем значение корневого, то пытаемся поместить новый узел в левое поддерево
+        if ($newNode->value < $root->value) {
+            //Если корневой узел левого дерева не занят, то помещаем в него новый узел
+            if (is_null($root->leftTree)) {
+                $root->leftTree = $newNode;
+            } else {
+                //Рекурсивно пытаемся добавить новый узел в левое поддерево
+                $this->_addNode($root->leftTree, $newNode);
+            }
+        }
 
-        if ($item->value < $node->value) {
-            if (is_null($node->leftTree)) {
-                $node->leftTree = $item;
+        //Если значение нового узла больше, чем значение корневого, то пытаемся поместить новый узел в правое поддерево
+        if ($newNode->value > $root->value) {
+            //Если корневой узел правого дерева не занят, то помещаем в него новый узел
+            if (is_null($root->rightTree)) {
+                $root->rightTree = $newNode;
             } else {
-                $this->_addNode($node->leftTree, $item);
+                //Рекурсивно пытаемся добавить новый узел в правое поддерево
+                $this->_addNode($root->rightTree, $newNode);
             }
         }
-        if ($item->value > $node->value) {
-            if (is_null($node->rightTree)) {
-                $node->rightTree = $item;
-            } else {
-                $this->_addNode($node->rightTree, $item);
-            }
-        }
-        if ($item->value === $node->value) {
+
+        //Препятствуем добавлению в дерево дубликатов узлов
+        if ($newNode->value === $root->value) {
             echo "Ошибка! Такой узел уже присутствует в дереве!";
         }
     }
 
+    //Перебираем узлы дерева в глубину слева направо
     public function fromLeftToRight($dataHadler)
     {
-        $this->inOrder($this->node, $dataHadler);
+        $this->inOrder($this->root, $dataHadler);
 
     }
 
+    //Добавляем узлы дерева в массив (при переборе)
     public function dataHandler(BinaryNode $node)
     {
         $this->array[] = $node->value;
     }
 
-    private function inOrder($node, $dataHadler)
+    //Задаём порядок обхода узлов дерева при переборе
+    private function inOrder($root, $dataHadler)
     {
-        if (is_null($node)) {
+        //Условие выхода из рекурсии - корневой узел не существует
+        if (is_null($root)) {
             return;
         }
-        $this->inOrder($node->leftTree, $dataHadler);
-        $dataHadler($node->value);
-        $this->inOrder($node->rightTree, $dataHadler);
+        $this->inOrder($root->leftTree, $dataHadler);
+        $dataHadler($root->value);
+        $this->inOrder($root->rightTree, $dataHadler);
     }
 
+    //Выводим список значений узлов дерева, полученный в результате перебора
     public function getNodeList()
     {
         $this->fromLeftToRight((function ($value) {
@@ -90,6 +108,7 @@ class BinaryTree
         }));
     }
 
+    //Возвращаем массив значений узлов дерева, полученный в результате перебора
     public function getNodeArray()
     {
         $this->array=[];
@@ -114,7 +133,7 @@ $binaryTree->addNode(12);
 $binaryTree->addNode(15);
 
 
-var_dump($binaryTree->node);
+var_dump($binaryTree->root);
 /*$binaryTree->fromLeftToRight(function ($value) {
     var_dump($value);
 });*/
